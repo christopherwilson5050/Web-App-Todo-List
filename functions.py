71eeae17f8be39d50aef4a330e1b7f4a6862df6c
chris <christopherwilson5050@gmail.com>
2025-04-10 21:28:31 +0100

    Fist bug stopping deployment

--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
-def get_todos(filepath=r"C:\Users\chris\PycharmProjects\PythonProject\todowebapp\todostore.txt"):
+def get_todos(filepath="todostore.txt"):
     with open(filepath, 'r') as file_funct:
         todos_funct = file_funct.readlines()
 
     return todos_funct
 
-def write_todos(local_todos,filepath=r"C:\Users\chris\PycharmProjects\PythonProject\todowebapp\todostore.txt"):
+def write_todos(local_todos,filepath="todostore.txt"):
     with open(filepath, 'w') as file:
         file.writelines(local_todos)
 
